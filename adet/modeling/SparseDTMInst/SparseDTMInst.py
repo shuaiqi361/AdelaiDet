42a7e434fbf0ca5c41f03a12e4c48847685c79da
shuaiqi361 <liukeyi1993@gmail.com>
2021-05-20 20:44:56 -0400

    remove residual code

--- adet/modeling/SparseDTMInst/SparseDTMInst.py
+++ adet/modeling/SparseDTMInst/SparseDTMInst.py
@@ -346,8 +346,9 @@ class DTInstHead(nn.Module):
             # mask_code_prediction = self.mask_pred(mask_tower)
             # mask_tower_cat = torch.cat([mask_tower, cls_tower, bbox_tower], dim=1)
             mask_tower_cat = mask_tower + cls_tower + bbox_tower
+            # mask_tower_cat = mask_tower + cls_tower
             residual_mask = self.residual(mask_tower_cat)
-            mask_reg.append(self.mask_pred(residual_mask + mask_tower))
+            mask_reg.append(self.mask_pred(residual_mask))
 
             # cls_tower_cat = torch.cat([mask_tower, cls_tower], dim=1)
             logits.append(self.cls_logits(cls_tower))
