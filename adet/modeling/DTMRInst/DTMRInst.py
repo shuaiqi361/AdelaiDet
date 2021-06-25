3c797d412ef463b67f917379ba977fc9dd0830bb
shuaiqi361 <liukeyi1993@gmail.com>
2021-06-25 17:38:58 -0400

    debug

--- adet/modeling/DTMRInst/DTMRInst.py
+++ adet/modeling/DTMRInst/DTMRInst.py
@@ -284,7 +284,7 @@ class DTMRInstHead(nn.Module):
         )
 
         self.refine_output = nn.Sequential(
-            nn.Conv2d(self.mask_size ** 2 + in_channels * 2, in_channels * 2, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(self.mask_size ** 2, self.mask_size ** 2, kernel_size=1, stride=1, padding=0),
             nn.Sigmoid(),
         )
 
